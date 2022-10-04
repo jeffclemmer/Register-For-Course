832bfec69d83db4836d572862f5c9d3d9bd1569d
jeff clemmer <clemmer.jeff@gmail.com>
2022-10-03 18:02:16 -0700

    couple more tests, you know, for science

--- index.test.js
+++ index.test.js
@@ -32,6 +32,21 @@ let catalog = [
       oneOf: ["shortstories101"],
     },
   },
+  {
+    // unique integer
+    id: 1,
+
+    // string, unique course name
+    name: "science200",
+
+    prereqs: {
+      // array of other course IDs that all need to be completed before this course
+      allOf: ["science101", "science102", "science103"],
+
+      // array of other course IDs where at least one needs to be completed before this course - could be empty
+      oneOf: [],
+    },
+  },
 ];
 
 test("can a user register for math200 - yes", () => {
@@ -52,5 +67,19 @@ test("can a user register for english200 - no", () => {
   let completedCourseIds = ["english101", "shortstories101"];
 
   let res = canRegisterForCourse(catalog, "english200", completedCourseIds);
+  expect(res).toBe(false);
+});
+
+test("can a user register for science200 - yes", () => {
+  let completedCourseIds = ["science101", "science102", "science103"];
+
+  let res = canRegisterForCourse(catalog, "science200", completedCourseIds);
+  expect(res).toBe(true);
+});
+
+test("can a user register for science200 - no", () => {
+  let completedCourseIds = ["science103", "science101"];
+
+  let res = canRegisterForCourse(catalog, "science200", completedCourseIds);
   expect(res).toBe(false);
 });
